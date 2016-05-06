57a21feccd071e0f1934a0d5515d0c3c293437be
Joshua Masland-Sarani <j.masland@gmail.com>
2016-05-06 13:32:25 -0400

    set up cable

--- config/initializers/request_forgery_protection.rb
+++ config/initializers/request_forgery_protection.rb
@@ -2,3 +2,5 @@
 
 # Enable origin-checking CSRF mitigation.
 Rails.application.config.action_controller.forgery_protection_origin_check = true
+
+Rails.application.config.action_cable.allowed_request_origins = ['http://localhost:3000']
