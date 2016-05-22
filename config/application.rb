fc84052791579e4e0975a7174b38d03e0fae2ff2
Joshua Masland-Sarani <j.masland@gmail.com>
2016-05-21 21:59:58 -0400

    stuff

--- config/application.rb
+++ config/application.rb
@@ -18,6 +18,9 @@ Bundler.require(*Rails.groups)
 
 module Bitter
   class Application < Rails::Application
+
+    config.action_cable.mount_path = '/websocket'
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
