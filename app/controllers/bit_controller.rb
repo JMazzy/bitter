f785484d6024a3d7602be9e7d3a13ec046c515ec
Joshua Masland-Sarani <j.masland@gmail.com>
2016-06-12 14:07:39 -0400

    bit controller

--- app/controllers/bit_controller.rb
+++ app/controllers/bit_controller.rb
@@ -1,2 +1,33 @@
 class BitController < ApplicationController
+
+  def index
+    @bits = Bit.all
+  end
+
+
+  def create
+    @bit = current_user.bits.build( bit_params )
+    if @bit.save
+
+    else
+
+    end
+  end
+
+
+  def destroy
+    @bit = current_user.bits.find( params[:id] )
+    if @bit.destroy
+
+    else
+
+    end
+  end
+
+  private
+
+  def bit_params
+    params.require(:bit).permit(:data)
+  end
+
 end
